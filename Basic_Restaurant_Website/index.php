20dc4a09681d26f8edcd44c46dd917a833e66520
Salekin <serajushsalekin@gmail.com>
2017-09-30 23:20:31 +0600

    tag added

---
+++ Basic_Restaurant_Website/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="banner">
+			
+		</div><!--banner-->
+		<div class="nav">
+			
+		</div><!--nav-->
+		<div class="content">
+			<div class="footer">
+				<div class="colum three">
+					
+				</div><!--colum three-->
+				<div class="colum three">
+					
+				</div><!--colum three-->
+				<div class="colum three last">
+					
+				</div><!--colum three last-->
+				
+			</div><!--footer-->
+			
+		</div><!--content-->
+		
+	</div> <!--wrapper-->
+</body>
+</html>
