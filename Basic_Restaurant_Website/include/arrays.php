ea3796152a64584907b9e08a0b80b7044c5d8bbe
Salekin <serajushsalekin@gmail.com>
2017-10-01 17:15:29 +0600

    added team members

--- Basic_Restaurant_Website/include/arrays.php
+++ Basic_Restaurant_Website/include/arrays.php
@@ -19,4 +19,35 @@
 		),
 	);
 
+	$team_members = array(
+					
+						array(
+						
+								"name"			=> "Frankie III",
+								"position"		=> "Owner",
+								"bio"			=> "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a mean fritatta!",
+								"img"			=> "frankie"
+					
+							 ),
+						
+						array(
+					
+								"name"			=> "Francis",
+								"position"		=> "General Manager",
+								"bio"			=> "Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don't miss her Margherita Mondays!",
+								"img"			=> "francis"
+					
+							 ),
+						
+						array(
+					
+								"name"			=> "Carlos",
+								"position"		=> "Head Chef",
+								"bio"			=> "Carlos is the epitome of the phrase &ldquo;Don't judge a book by it's cover&rdquo; &mdash; You simply cannot find a better chef.",
+								"img"			=> "carlos"
+					
+							 ),
+					
+				   );
+
   ?>
--- Basic_Restaurant_Website/team.php
+++ Basic_Restaurant_Website/team.php
@@ -3,3 +3,25 @@ define('TITLE', "Team | DemoHustle");
 
 	include 'include/header.php'; 
  ?>
+ <div id="team-members" class="cf">
+ 	<h1>Our Team at Franklin's</h1>
+		<p>We're small, but mighty. Franklin's Fine Dining has been a family-owned and run business since the dirty thirties, and we're proud of it! When you get these three together, you never know what can happen. But you can count on one thing: <strong>The best food you've ever had. Ever.</strong></p>
+		
+		<hr>
+
+		<?php foreach ($team_members as $member) { ?>
+			
+			<div class="member">
+				<img src="img/<?php echo $member["img"]; ?>.png">
+				<h2><?php echo $member["name"]; ?></h2>
+				<p><?php echo $member["bio"]; ?></p>
+			</div><!-- member -->
+		
+		<?php } ?>
+ 	
+ </div>
+ <hr>
+
+ <?php
+ 	include 'include/footer.php'; 
+  ?>
